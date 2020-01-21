03250bcd20fdc5247febd37463f0916347858b51
Aquil Hassan <34624268+Acquil@users.noreply.github.com>
2020-01-21 15:20:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import pymongo
+from flask import request
+from flask import jsonify
+from bson.json_util import dumps
+import flask
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+client = pymongo.MongoClient("mongodb+srv://user01:bl4ck4dd3r@cluster0-kooqx.mongodb.net/test?retryWrites=true&w=majority")
+db = client.sample_airbnb
+collection = db.listingsAndReviews
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/name/', methods=['GET'])
+def api_all():
+    name = request.args.get('name')
+    result = collection.find({"name":name})
+    return jsonify(dumps(result))
+
+
+app = Flask(__name__)
+log = app.logger
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
