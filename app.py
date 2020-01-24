23747932cdcfaebb702a1571422c4fbcc597d258
Aquil Hassan <34624268+Acquil@users.noreply.github.com>
2020-01-24 10:00:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def home():
 
 
 # A route to return all of the available entries in our catalog.
-@app.route('/api/city/', methods=['GET'])
-def api_all():
+@app.route('/api/get/city/', methods=['GET'])
+def get_city():
     name = request.args.get('name')
     result = collection.find({"city":name})
     op = []
@@ -30,6 +30,48 @@ def api_all():
         op.append(obj)    
     
     return jsonify(op)
+
+@app.route('/api/post/insert/', methods=['POST'])
+def insert_city_with_check():
+    city = request.args.get('city')
+    loc_x = request.args.get('loc_x')
+    loc_y = request.args.get('loc_y')
+    pop = request.args.get('pop')
+    state = request.args.get('state')
+    Zip = request.args.get('zip')
+
+    if city and loc_x and loc_y and pop and state and Zip:
+        result = collection.find({"city":city})
+        op = []
+        for obj in result:
+            obj.pop('_id', None)
+            obj.update({"ERROR": "City already exists"})
+            op.append(obj)
+        if op:
+             return jsonify(op)
+        else:
+            test_json = {"city":city,"loc":{"x":loc_x,"y":loc_y},"pop":pop,"state":state,"zip":Zip}
+            post_id = collection.insert_one(test_json).inserted_id
+            return "<h1> Inserted Successfully "
+    else:
+        return "<h1> Please give all keys "
+
+@app.route('/api/put/insert/', methods=['PUT'])
+def insert_city_without_check():
+    city = request.args.get('city')
+    loc_x = request.args.get('loc_x')
+    loc_y = request.args.get('loc_y')
+    pop = request.args.get('pop')
+    state = request.args.get('state')
+    Zip = request.args.get('zip')
+
+    if city and loc_x and loc_y and pop and state and Zip:
+       test_json = {"city":city,"loc":{"x":loc_x,"y":loc_y},"pop":pop,"state":state,"zip":Zip}
+       post_id = collection.insert_one(test_json).inserted_id
+       return "<h1> Inserted Successfully "
+
+    else:
+        return "<h1> Please give all keys "
 
 
 if __name__ == '__main__':
